c43b060ef1d568cf42fe9987c2057afc2e8ed402
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2024-08-10 18:16:35 +0200

    operations

---
+++ 2-operations.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+# Disable eager execution to use graph mode
+tf.compat.v1.disable_eager_execution()
+
+# Get the default graph
+graph = tf.compat.v1.get_default_graph()
+
+# Get the list of operations in the graph
+operations = graph.get_operations()
+
+
+
+a = tf.constant(10,  name="a")
+b = tf.constant(20, name="b")
+c = tf.add(a,b, name="c")
+d=tf.multiply(a,c, name="d")
+e=tf.multiply(d,c, name="e")
+
+sess = tf.compat.v1.Session()
+
+for op in graph.get_operations():
+    print(op)
